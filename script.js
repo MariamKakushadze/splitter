903fac695b5c6d714ee35ac25d5884c8e7b3c707
Mariam Kakushadze <mariam-kakushadze@Mariams-MacBook-Air.local>
2022-10-14 17:22:04 +0400

    fixed some bugs

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const personNum=document.querySelector('#people');
 const tipAmount=document.querySelector('.final-tip');
 const totalAmount=document.querySelector('.final-total');
 const resetBtn=document.querySelector('.reset');
-
+const error=document.querySelector('.error-msg');
 
 const reset=function(){
 tipAmount.innerHTML=`$0.00`;
@@ -15,43 +15,52 @@ totalAmount.innerHTML=`$0.00`;
 bill.value='';
 customTip.value='';
 personNum.value='';
+error.style.display="none";
+personNum.style.border = "none";
+bill.style.border = "none";
+customTip.style.border = "none";
 }
 
-function personVal(){
-    let format = /^[1-9]+[0-9]*$/;   
-        if(!personNum.value.match(format)){
-          personNum.style.border = "2px solid red";
-          return false;
-        }else{
-          personNum.style.border = "none"; 
-          return true;
+personNum.addEventListener('input', function(){
+  if (personNum.value%1!=0){
+    personNum.value=Math.round(personNum.value);
+    error.style.display="none";
+    personNum.style.border = "none";
+  }else if(personNum.value==0){
+    personNum.style.border = "2px solid red";
+    error.style.display="block";
+    error.innerHTML="Can't be zero!"
+  }else if(personNum.value<0){
+    personNum.style.border = "2px solid red";
+    error.style.display="block";
+    error.innerHTML="Can't be negative!"
+  }else{
+    personNum.style.border = "none";
+    error.style.display="none";
+    calculateTip();
+  }
+});
+
+bill.addEventListener('input',function(){
+   if(bill.value<=0){
+    bill.style.border = "2px solid red";
+   }else{
+    bill.style.border = "none";
+    calculateTip();
    }
- }
+});
 
- function decimalVal(){
-    let format=/^(?!0*[.,]0*$|[.,]0*$|0*$)\d+[,.]?\d{0,2}$/;
-    if(!bill.value.match(format)){
-        bill.style.border = "2px solid red";
-        return false;
-    }else{
-        bill.style.border = "none";
-        return true;
-    }
- }
- function tipVal(){
-    let format=/^(?!0*[.,]0*$|[.,]0*$|0*$)\d+[,.]?\d{0,2}$/;
-    if(!customTip.value.match(format)){
-        customTip.style.border = "2px solid red";
-        return false;
-    }else{
-        customTip.style.border = "none";
-        return true;
-    }
- }
-
+customTip.addEventListener('input', function(){
+  if(customTip.value<=0){
+    customTip.style.border = "2px solid red";
+  }else{
+    customTip.style.border = "none";
+    calculateTip();
+  }
+});
 
  function calculateTip(){
-    if(bill.value>0 && personNum.value>0 && customTip.value>0){
+  if(bill.value>0 && personNum.value>0 && customTip.value>0){
   const percent=customTip.value/100*bill.value;
   const total=Number(bill.value)+percent;
   const personTip=(percent/personNum.value).toFixed(2);
@@ -59,16 +68,16 @@ function personVal(){
 
   tipAmount.innerHTML=`$${personTip}`;
   totalAmount.innerHTML=`$${personTotal}`;
- }
- personVal();
- decimalVal();
- tipVal();
+     fontSize();
+  
+  }
 }
-
 
  for(let i=0; i<tip.length; i++){
     tip[i].addEventListener('click',function(){
     if(bill.value>0 && personNum.value>0){
+      if( personNum.style.border != "2px solid red" &&
+      bill.style.border != "2px solid red" ){
         customTip.style.border = "none";
   const percent= tip[i].value/100*bill.value;
   const total=Number(bill.value)+percent;
@@ -77,16 +86,46 @@ function personVal(){
 
   tipAmount.innerHTML=`$${personTip}`;
   totalAmount.innerHTML=`$${personTotal}`;
+  fontSize();
     }
+  }
  });
- 
 }
 
-
-customTip.addEventListener('input',calculateTip);
-bill.addEventListener('input',calculateTip);
-personNum.addEventListener('input',calculateTip);
-
+const fontSize=function(){
+  if(innerWidth>=1400){
+    if(tipAmount.innerHTML.length>8 || totalAmount.innerHTML.length>8){
+      tipAmount.style.fontSize="36px";
+      totalAmount.style.fontSize="36px";
+   } 
+   if(tipAmount.innerHTML.length>11 || totalAmount.innerHTML.length>11){
+      tipAmount.style.fontSize="26px";
+      totalAmount.style.fontSize="26px";
+   } 
+   if(tipAmount.innerHTML.length>16 || totalAmount.innerHTML.length>16){
+      tipAmount.style.fontSize="16px";
+      totalAmount.style.fontSize="16px";
+  }
+  if (tipAmount.innerHTML.length<9 || totalAmount.innerHTML.length<9){
+      tipAmount.style.fontSize="48px";
+      totalAmount.style.fontSize="48px";
+     }
+  }
+  if(innerWidth<1400){
+    if(tipAmount.innerHTML.length>9 || totalAmount.innerHTML.length>9){
+      tipAmount.style.fontSize="26px";
+      totalAmount.style.fontSize="26px";
+  }
+  if(tipAmount.innerHTML.length>11 || totalAmount.innerHTML.length>11){
+    tipAmount.style.fontSize="14px";
+    totalAmount.style.fontSize="14px";
+  }
+  if(tipAmount.innerHTML.length<10 || totalAmount.innerHTML.length<10){
+    tipAmount.style.fontSize="32px";
+    totalAmount.style.fontSize="32px";
+   }
+ }
+}
 
 resetBtn.addEventListener('click',reset);
 
